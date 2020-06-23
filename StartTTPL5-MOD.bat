c2d657c4ba28879aed6ef006ec39749cd1d49089
Phil <phil@tatm.com>
2020-06-24 01:01:57 +0200

    Add files via upload

---
+++ StartTTPL5-MOD.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+
+SET MYSCRIPT=%0
+Rem echo on
+SET DIR_MYSCRIPT=%~dp0
+
+Rem call :getport COMPORT
+set /p COMPORT="Enter COM port (for ex: 'COM53:'): "
+if "%COMPORT%" == "" goto ErrorCOM
+if "%COMPORT%" == "CON:" goto ErrorCOM
+
+SET TT_MACRO_FILE=%DIR_MYSCRIPT%\ttlMacro_Startup_L5.ttl
+SET TT_MACRO_EXE=%DIR_MYSCRIPT%\ttpmacro.exe
+echo comport : %COMPORT%
+@echo on
+%TT_MACRO_EXE%  %TT_MACRO_FILE% %COMPORT%
+goto end
+
+:ErrorCOM
+echo "Error! No Com port available !"
+goto End
+
+
+:getport comName
+    FOR /F "tokens=4 USEBACKQ" %%F IN (`mode`) DO (
+		echo %%F
+        if %%F NEQ CON: SET %1=%%F
+    )
+    echo Return : %1
+
+GOTO :EOF
+
+:End
+endlocal
+Rem pause
